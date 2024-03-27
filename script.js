8f32557b626b7900b0566facda7c569d90e01dda
StanescuI <162870985+StanescuI@users.noreply.github.com>
2024-03-27 13:57:35 +0200

    Recreated the game with a different approach
    Got rid of repetitive code;
    Reworked the winning state check by implementing array destructuring for checking every winning possibility.
    Fixed console errors that occurred after   the end-game state.

--- script.js
+++ script.js
@@ -1,105 +1,69 @@
-let playerTurn = 1;
-let totalClicks = 0;
+let scoreBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+let playerTurn = "X";
 let result = document.getElementById("result");
-let scoreBoard = [];
-for (let i = 0; i <= 7; ++i) {
-    scoreBoard[i] = 3;
-}
+let totalClicks = 0;
 
-function setElement(pressedButton) {
-    let pressed = document.getElementById(pressedButton);
-    if (playerTurn === 1 && pressed.innerText === "") {
-        pressed.innerText = "X";
-        playerTurn = 0;
+function markButton(clicked) {
+    let buttonId = document.getElementById(clicked);
+    let idToText = parseInt(clicked);
+    
+    if (scoreBoard[idToText - 1] === 0) {
         ++totalClicks;
-        result.innerText = "0 turn";
-        addPoints(pressed);
-    } else if (playerTurn === 0 && pressed.innerText === "") {
-        pressed.innerText = "0";
-        playerTurn = 1;
-        ++totalClicks;
-        result.innerText = "X turn";
-        decreasePoints(pressed);
-    }
-    if (totalClicks > 8) {
-        result.innerText = "The game ended with a Tie !"
-        finishedGame();
-    }
-}
-
-function addPoints(clicked) {
-    if (clicked.classList.contains("firstLine")) {
-        ++scoreBoard[0];
-    } else if (clicked.classList.contains("secLine")) {
-        ++scoreBoard[1];
-    } else {
-        ++scoreBoard[2];
-    }
-    if (clicked.classList.contains("firstCol")) {
-        ++scoreBoard[3];
-    } else if (clicked.classList.contains("secCol")) {
-        ++scoreBoard[4];
-    } else {
-        ++scoreBoard[5];
-    }
-    if (clicked.classList.contains("firstDiag")) {
-        ++scoreBoard[6];
-    } else if (clicked.classList.contains("secDiag")) {
-        ++scoreBoard[7];
-    }
-    for (let i = 0; i <= 7; ++i) {
-        if (scoreBoard[i] === 6) {
-            result.innerText = "X won!";
-            finishedGame();
+        if (playerTurn === "X") {
+            buttonId.innerText = "X";
+            scoreBoard[idToText - 1] = 1;
+            playerTurn = "0";
+            result.innerText = "0 Turn";
+            checkWin(scoreBoard, "X");
+        } else {
+            buttonId.innerText = "0";
+            scoreBoard[idToText - 1] = 2;
+            playerTurn = "X";
+            result.innerText = "X Turn";
+            checkWin(scoreBoard, "0");
         }
     }
 }
 
-function decreasePoints(clicked) {
-    if (clicked.classList.contains("firstLine")) {
-        --scoreBoard[0];
-    } else if (clicked.classList.contains("secLine")) {
-        --scoreBoard[1];
-    } else {
-        --scoreBoard[2];
-    }
-    if (clicked.classList.contains("firstCol")) {
-        --scoreBoard[3];
-    } else if (clicked.classList.contains("secCol")) {
-        --scoreBoard[4];
-    } else {
-        --scoreBoard[5];
-    }
-    if (clicked.classList.contains("firstDiag")) {
-        --scoreBoard[6];
-    } else if (clicked.classList.contains("secDiag")) {
-        --scoreBoard[7];
-    }
-    for (let i = 0; i <= 7; ++i) {
-        if (scoreBoard[i] === 0) {
-            result.innerText = "0 won!";
+function checkWin(array, winner) {
+    let winningCombinations = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+        [1, 4, 7],
+        [2, 5, 8],
+        [3, 6, 9],
+        [1, 5, 9],
+        [3, 5, 7]
+    ];
+    for (let combination of winningCombinations) {
+        let [index1, index2, index3] = combination;
+        let value1 = array[index1 - 1];
+        let value2 = array[index2 - 1];
+        let value3 = array[index3 - 1];
+        if (value1 !== 0 && value1 === value2 && value2 === value3) {
+            result.innerText = winner + " won";
             finishedGame();
         }
     }
 }
 
 function finishedGame() {
-    playerTurn = 3;
+    playerTurn = "Z";
     totalClicks = 0;
+    scoreBoard = [3, 3, 3, 3, 3, 3, 3, 3, 3];
     let playAgain = document.createElement('button');
     playAgain.textContent = 'Play again!';
     playAgain.id = "rematch";
     playAgain.onclick = function() {
-        for (let i = 0; i <= 7; ++i) {
-            scoreBoard[i] = 3;
-        }
-        for (let i = 0; i <= 8; ++i) {
-            let allButtons = document.querySelectorAll("button");
-            allButtons.forEach(button => {
-                button.innerText = "";
-            });
+    scoreBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    for (let i = 0; i <= 8; ++i) {
+        let allButtons = document.querySelectorAll("button");
+        allButtons.forEach(button => {
+            button.innerText = "";
+        });
         result.innerText = "X turn";
-        playerTurn = 1;
+        playerTurn = "X";
         }
         playAgain.remove();
     }
